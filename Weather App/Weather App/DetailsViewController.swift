0288bf47d508af63d52bed8fe48a6d901e118204
Amit Miskeen <a_miskeen@fanshaweonline.ca>
2023-08-02 22:45:25 -0400

    Initial commit: Add Weather App project

---
+++ Weather App/Weather App/DetailsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Weather App
+//
+//  Created by Abhay Miskeen on 2023-08-02.
+//
+
+import UIKit
+
+class DetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
