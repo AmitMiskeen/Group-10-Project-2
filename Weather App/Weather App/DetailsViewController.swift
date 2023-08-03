71e0f1f11e4413ac577586984e44631894cb1888
Amit Miskeen <a_miskeen@fanshaweonline.ca>
2023-08-02 23:34:19 -0400

    First Page

--- Weather App/Weather App/DetailsViewController.swift
+++ Weather App/Weather App/DetailsViewController.swift
@@ -8,12 +8,40 @@
 import UIKit
 
 class DetailsViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var locationLabel: UILabel!
+    
+    @IBOutlet weak var temperatureLabel: UILabel!
+    
+    
+    @IBOutlet weak var weatherConditionLabel: UILabel!
+    @IBOutlet weak var weatherImage: UIImageView!
+    
+    @IBOutlet weak var searchBar: UISearchBar!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func tempToggleButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func searchButton(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func logsButton(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func cancelButton(_ sender: UIBarButtonItem) {
+    }
+    
+    
+    @IBAction func addLocationButton(_ sender: UIBarButtonItem) {
+    }
 }
 
