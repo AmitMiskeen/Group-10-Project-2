f1c39a5f8bd930649c42f405d19a16e136b2c36c
Amit Miskeen <a_miskeen@fanshaweonline.ca>
2023-08-03 18:23:44 -0400

    Fixed error in first page

--- Weather App/Weather App/DetailsViewController.swift
+++ Weather App/Weather App/DetailsViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  DetailsViewController.swift
 //  Weather App
 //
 //  Created by Abhay Miskeen on 2023-08-02.
@@ -10,30 +10,32 @@ import UIKit
 class DetailsViewController: UIViewController {
     
     
+    @IBOutlet weak var conditionLabel: UILabel!
+    
+    @IBOutlet weak var weatherImageDisplay: UIImageView!
+    
+    
+    @IBOutlet weak var tempertureLabel: UILabel!
+    
+    
     @IBOutlet weak var locationLabel: UILabel!
     
-    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var searchLocation: UISearchBar!
     
     
-    @IBOutlet weak var weatherConditionLabel: UILabel!
-    @IBOutlet weak var weatherImage: UIImageView!
-    
-    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var conditionDisplay: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
-
-
-    @IBAction func tempToggleButton(_ sender: UIButton) {
+    
+    @IBAction func temperatureType(_ sender: UIButton) {
     }
+    
     
     @IBAction func searchButton(_ sender: UIButton) {
-    }
-    
-    
-    @IBAction func logsButton(_ sender: UIButton) {
     }
     
     
@@ -43,5 +45,9 @@ class DetailsViewController: UIViewController {
     
     @IBAction func addLocationButton(_ sender: UIBarButtonItem) {
     }
+    
+    @IBAction func logsButton(_ sender: UIButton) {
+    }
+    
 }
 
